9e36470c2925298d7ff3c825fa33a93d249fb94a
Kevin Teisseire <teisseire.kevin@gmail.com>
2026-05-15 15:23:10 +0200

    fix cloudinary API addresses

--- Backend/app.py
+++ Backend/app.py
@@ -17,7 +17,9 @@ load_dotenv()
 
 cloudinary.config(
     cloud_name = os.getenv("CLOUD_NAME"),
-    api_key = os.getenv("API_KEY")
+    api_key = os.getenv("API_KEY"),
+    api_secret = os.getenv("API_SECRET"),
+    secure = True
 )
 
 app = Flask(__name__)
