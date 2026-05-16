240ab06805dda131d9a8f4289875e50e39c72867
Kevin Teisseire <teisseire.kevin@gmail.com>
2026-05-16 10:40:01 +0200

    add isloading variable to fix duplicate forum

--- docs/auth.js
+++ docs/auth.js
@@ -1,11 +1,11 @@
 import { loginWrapper, signupWrapper, main, profileSection, profileInfos, navBar, pageWrapper, error_wrongCred,
-        error_userExists, profileMenu, signupBtn, loginBtn, signupTxt, loginTxt, authPop } from "./dom.js"
-import { show, hide, toggleSections, cleanInputs } from "./UI.js"
-import { login, signUp } from "./API.js"
-import { loadAndRenderForum } from "./forum.js"
-import { STATE } from "./state.js"
-import { displayInfos } from "./profile.js"
-import { showMenu, hideMenu } from "./navigation.js"
+        error_userExists, profileMenu, signupBtn, loginBtn, signupTxt, loginTxt, authPop } from "./dom.js";
+import { show, hide, toggleSections, cleanInputs } from "./UI.js";
+import { login, signUp } from "./API.js";
+import { loadAndRenderForum } from "./forum.js";
+import { STATE } from "./state.js";
+import { displayInfos } from "./profile.js";
+import { showMenu, hideMenu } from "./navigation.js";
 
 /* ========================
         Authentification
@@ -13,49 +13,54 @@ import { showMenu, hideMenu } from "./navigation.js"
 
 // Login text link to signup
 signupTxt.addEventListener('click', () => {
-    toggleSections([loginWrapper], [signupWrapper])
+    toggleSections([loginWrapper], [signupWrapper]);
 })
 
 // Signup text to login
 loginTxt.addEventListener('click', () => {
-    toggleSections([signupWrapper], [loginWrapper])
+    toggleSections([signupWrapper], [loginWrapper]);
 })
 
 // Login 
 export function setCurrentUser(user){
     STATE.currentUser = user;
-    localStorage.setItem("user", JSON.stringify(user))
+    localStorage.setItem("user", JSON.stringify(user));
 }
 
 async function logUserIn(){
-    const email = document.getElementById('login-email-ipt').value;
-    const passWord = document.getElementById('login-pw-ipt').value;
-    // Check infos missing
-    if (!email || !passWord){
-        alert("Some fields are missing")
-    // If all infos
-    } else {
+    let isLoading = false;
+    if (isloading) return;
+    isloading = true;
+    try{
+        const email = document.getElementById('login-email-ipt').value;
+        const passWord = document.getElementById('login-pw-ipt').value;
+        // Check infos missing
+        if (!email || !passWord){
+            alert("Some fields are missing");
+            return;
+        }
         const res = await login(email, passWord);
         // User found in DB
-        if (res.data.status === "success"){
-            console.log('success')
-            setCurrentUser(res.user)
+        if (res.data.status === "success"){ 
+            setCurrentUser(res.user);
             displayInfos(STATE.currentUser);
             // Load forum messages
-            await loadAndRenderForum(STATE.forumPage)
-            toggleSections([authPop], [main, profileSection, profileInfos, navBar])
-            pageWrapper.style.justifyContent = ''
-            showMenu(profileMenu)
-            console.log(`auth-logUserIn: state = ${STATE}`)
+            await loadAndRenderForum(STATE.forumPage);
+            toggleSections([authPop], [main, profileSection, profileInfos, navBar]);
+            pageWrapper.style.justifyContent = '';
+            showMenu(profileMenu);
+            console.log(`auth-logUserIn: state = ${STATE.currentUser}`);
         // User not found in DB
         } else {
-            show(error_wrongCred)
+            show(error_wrongCred);
         }
-    };
+    } finally {
+        isloading = false;
+    }
 }
 
 loginBtn.addEventListener("click", () => {
-    logUserIn()
+    logUserIn();
 });
 
 // Signup button
@@ -71,15 +76,15 @@ async function signUserUp(){
     } else {
         const res = await signUp(firstName, lastName, email, passWord);
         if (res.data.status === "success"){
-            setCurrentUser(res.user)
-            toggleSections([signupWrapper], [loginWrapper])
-            await loadAndRenderForum(STATE.forumPage)
+            setCurrentUser(res.user);
+            toggleSections([signupWrapper], [loginWrapper]);
+            await loadAndRenderForum(STATE.forumPage);
         } else if (res.data.status === "error" && res.data.message === "user exists"){
-           show(error_userExists)
+           show(error_userExists);
         }
     }
 }
 
 signupBtn.addEventListener("click", () => {
-    signUserUp()
+    signUserUp();
 });
