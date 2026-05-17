40dc99a418c2a440b3597222685ce15da7847e8d
Kevin Teisseire <teisseire.kevin@gmail.com>
2026-05-17 21:09:42 +0200

    default image on forum posts

--- docs/forum.js
+++ docs/forum.js
@@ -1,5 +1,7 @@
-import { forumBody, postNavPages, postStatusMessage, newPostTitle, newPostDescription, newPostPopup, createNewPostBtn,
-        sendNewPostBtn, cancelNewPostBtn, forumNextBtn, forumPrevBtn } from "./dom.js"
+import {
+    forumBody, postNavPages, postStatusMessage, newPostTitle, newPostDescription, newPostPopup, createNewPostBtn,
+    sendNewPostBtn, cancelNewPostBtn, forumNextBtn, forumPrevBtn
+} from "./dom.js"
 import { show, hide, toggleSections, cleanInputs } from "./UI.js"
 import { loadForum, post } from "./API.js"
 import { STATE } from "./state.js"
@@ -10,7 +12,7 @@ import { STATE } from "./state.js"
 =========================== */
 
 // Create existing posts HTML
-function createForumPostsHtml(element){
+function createForumPostsHtml(element) {
     // Card container
     const post = document.createElement("div")
     post.classList.add("large-card")
@@ -21,19 +23,19 @@ function createForumPostsHtml(element){
     postBodyLeft.classList.add("post-body-left")
     post.appendChild(postBodyLeft)
     // Left card side content
-        // User profile image
+    // User profile image
     const userImgWrapper = document.createElement("div")
     userImgWrapper.classList.add("small-image-wrapper")
     userImgWrapper.id = "post-user-img"
     userImgWrapper.style.backgroundImage = `url('${element.image_url}')`
     postBodyLeft.appendChild(userImgWrapper)
-        // User name
+    // User name
     const postName = document.createElement("h5")
     postName.classList.add("title-text-small")
     postName.id = "post-user-name"
     postName.textContent = element.first_name
     postBodyLeft.appendChild(postName)
-        // Creation date
+    // Creation date
     const postDate = document.createElement("p")
     postDate.classList.add("body-text-small")
     postDate.id = "post-date"
@@ -48,13 +50,13 @@ function createForumPostsHtml(element){
     const postBodyRight = document.createElement("div")
     postBodyRight.classList.add("post-body-right")
     post.appendChild(postBodyRight)
-        // Post title
+    // Post title
     const postTitle = document.createElement("h5")
     postTitle.classList.add("title-text-small")
     postTitle.id = "post-title"
     postTitle.textContent = element.title
     postBodyRight.appendChild(postTitle)
-        // Post description
+    // Post description
     const postDescription = document.createElement("p")
     postDescription.classList.add("body-text-small")
     postDescription.id = "post-description"
@@ -63,7 +65,7 @@ function createForumPostsHtml(element){
 }
 
 // Render forum 
-export function setForumParam(res){
+export function setForumParam(res) {
     Object.assign(STATE, {
         forumTotalPages: res.total_pages,
         forumPosts: res.posts,
@@ -71,7 +73,7 @@ export function setForumParam(res){
     })
 }
 
-export function renderForum(path=null){
+export function renderForum(path = null) {
     console.log(path)
     // Reset html
     forumBody.innerHTML = ''
@@ -79,12 +81,19 @@ export function renderForum(path=null){
     const start = (STATE.forumPage - 1) * 4;
     const end = STATE.forumPage * 4;
     // Generate visible posts
-    console.log(STATE.forumPosts)
-    console.log(Array.isArray(STATE.forumPosts))
-    if (STATE.forumPosts){
+    if (STATE.forumPosts) {
+        // Default image url
+        const defaultImageUrl = 'https://res.cloudinary.com/dndeflndh/image/upload/v1779044690/Capture_d_e%CC%81cran_2026-05-17_a%CC%80_21.04.43_rmc8mm.png'
+        // For each post on current page
         STATE.forumPosts.slice(start, end).forEach(el => {
+            // Set default if image = null
+            if (el.image_url === null){
+                el.image_url === defaultImageUrl
+            }
+            // Create posts HTML
             createForumPostsHtml(el)
         })
+        // Display current and max page
         postNavPages.textContent = `${STATE.forumPage} / ${Math.ceil(STATE.forumTotalPages)}`
     } else {
         postStatusMessage.textContent = 'No post in this forum yet'
@@ -92,7 +101,7 @@ export function renderForum(path=null){
 }
 
 // Save new post
-async function sendPost(){
+async function sendPost() {
     const res = await post(newPostTitle.value, newPostDescription.value, STATE.currentUser["user_id"])
     console.log(`sendPost(): ${res.status}`)
     return res
@@ -103,7 +112,7 @@ createNewPostBtn.addEventListener("click", () => {
     show(newPostPopup)
 })
 
-sendNewPostBtn.addEventListener("click", async() => {
+sendNewPostBtn.addEventListener("click", async () => {
     const newPost = await sendPost()
     setForumParam(newPost)
     renderForum()
@@ -112,37 +121,37 @@ sendNewPostBtn.addEventListener("click", async() => {
     cleanInputs([newPostDescription, newPostTitle])
 })
 
-cancelNewPostBtn.addEventListener("click", async() => {
+cancelNewPostBtn.addEventListener("click", async () => {
     toggleSections([newPostPopup], [postStatusMessage])
     cleanInputs([newPostDescription, newPostTitle])
 })
 
 // Forum pages navigation
-forumNextBtn.addEventListener("click", async() => {
-    if (STATE.forumPage < STATE.forumTotalPages){
-            STATE.forumPage++;
+forumNextBtn.addEventListener("click", async () => {
+    if (STATE.forumPage < STATE.forumTotalPages) {
+        STATE.forumPage++;
     }
     renderForum('forumNewtBtn');
 })
 
-forumPrevBtn.addEventListener("click", async() => {
-    if (STATE.forumPage > 1){
+forumPrevBtn.addEventListener("click", async () => {
+    if (STATE.forumPage > 1) {
         STATE.forumPage--;
     }
     renderForum();
-    
+
 })
 
 // Display new post validation message
-async function displayPostStatusMessage(status){
-    if (status === "success"){
+async function displayPostStatusMessage(status) {
+    if (status === "success") {
         postStatusMessage.style.color = 'green'
-        postStatusMessage.textContent = 'Post sent succesfully'   
+        postStatusMessage.textContent = 'Post sent succesfully'
     } else {
         postStatusMessage.style.color = 'red'
         postStatusMessage.textContent = 'Error : Post not sent'
     }
-    setTimeout(() =>{
-            postStatusMessage.textContent = ''
-        }, 2000)
+    setTimeout(() => {
+        postStatusMessage.textContent = ''
+    }, 2000)
 }
